f450025bbe49e2a5ee574de785375549d02229be
himalf <himal.fullel15@gmail.com>
2024-12-24 13:16:09 +0545

    style: style pagination buttons

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -52,7 +52,7 @@ const Products = () => {
       })}
     </main>
 {/* for pagination buttons  */}
-<div className="">
+<div className="flex justify-center items-center gap-3 p-2 my-2">
 {Array.from({length:totalPages},(_,index)=>(
      <button key={index} className={`px-4 py-2 rounded-md ${currentPage=== index+1?"bg-blue-500 text-white": "bg-gray-200 text-blue-500"}`}
      onClick={()=>{
