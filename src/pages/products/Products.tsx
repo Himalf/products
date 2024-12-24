3e964f51f17d28ae2518c43eafa60f9c73412bdc
himalf <himal.fullel15@gmail.com>
2024-12-24 16:09:29 +0545

    style: add responsive designs

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { IProduct } from "../../types/product";
 import { Link } from "react-router-dom";
+
 const Products = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -25,24 +26,24 @@ const Products = () => {
   const currentItems = products.slice(startIndex, endIndex);
 
   return (
-   <div className="p-5">
-     <main className="grid grid-cols-4 place-items-center place-content-center gap-10">
+   <div className="p-5 bg-gray-200 min-h-screen">
+     <main className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1  place-items-center place-content-center gap-10">
       {currentItems.map((val) => {
         return (
-          <section className="cursor-pointer p-2 border w-80 h-96 border-red-400 rounded-md">
-            <div className="w-56">
+          <section className="cursor-pointer p-4 border w-80 h-auto bg-white shadow-md rounded-md hover:shadow-lg transition-shadow duration-300">
+            <div className="w-full h-56 overflow-hidden flex justify-center items-center">
               <img
                 src={val.images[0]}
                 alt="product image"
                 className=" object-cover h-56 w-56"
               />{" "}
             </div>
-            <div className="font-semibold text-xl">{val.title}</div>
-           <div className="flex justify-between items-center" >
+            <div className="font-semibold text-xl mt-4">{val.title}</div>
+           <div className="flex justify-between items-center mt-2" >
            <div>${val.price}</div>
            <div>{val.rating}({val.reviews.length})</div>
            </div>
-            <div>
+            <div className="mt-4">
               <Link
                 to={`/products/${val.id}`}
                 className="bg-blue-600 text-white font-bold text-md px-5 py-1 rounded-md"
