606a9855d21a6056d7598ea90c5f9ee8d41efcf9
himalf <himal.fullel15@gmail.com>
2024-12-29 18:30:57 +0545

    feat: create  protected route component

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!auth?.token && localStorage.getItem("token")) {
+      navigate("/login");
+    }
+  }, [auth?.token, navigate]);
+  if (!auth?.token || !localStorage.getItem("token")) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="p-4 border border-red-500 bg-red-100 rounded">
+          <p className="text-red-500 font-bold">Unauthorized Access</p>
+          <button
+            onClick={() => navigate("/login")}
+            className="mt-2 bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Go to Login
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+}
