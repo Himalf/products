c61b0ee1eb83e377e771d524bc5acd8c486ec0aa
himalf <himal.fullel15@gmail.com>
2024-12-29 16:58:37 +0545

    feat: add authorization context file

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,2 +1,27 @@
 export const BASE_URL = "http://localhost:4000";
-export const setAuthToken = (token: string | null) => {};
+export const setAuthToken = (token: string | null) => {
+  if (token) {
+    localStorage.setItem("token", token);
+  } else {
+    localStorage.removeItem("token");
+  }
+};
+export const fetchWithAuth = async (
+  endPoint: string,
+  options: RequestInit = {}
+) => {
+  const token = localStorage.getItem("token");
+  const headers = {
+    ...(options.headers as Record<string, string>),
+    Authorization: token ? `${token}` : "",
+    "content-type": "application/json",
+  };
+  const response = await fetch(`${BASE_URL}/${endPoint}`, {
+    ...options,
+    headers,
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response.json();
+};
